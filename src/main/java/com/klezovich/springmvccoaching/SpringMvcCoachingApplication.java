9ffee7f7b0f69aef10c0d9a4a75244c7f35c77fc
aklezovics <arturs.klezovics@raisin.com>
2021-07-03 18:04:13 +0200

    Initial commit

---
+++ src/main/java/com/klezovich/springmvccoaching/SpringMvcCoachingApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.springmvccoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCoachingApplication.class, args);
+    }
+
+}
